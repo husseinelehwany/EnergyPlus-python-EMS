f306d3c98e8f6cd6b28f085c35e39d289315b406
Hussein Elehwany <husseinelehwany@cmail.carleton.ca>
2024-06-04 10:20:36 -0400

    notebook with more details model upgraded to 23.1

--- main.py
+++ main.py
@@ -2,17 +2,19 @@
 # It demonestrates how to run energyplus from a python script
 # Prepared by Hussein Elehwany 20/07/2023
 import sys
-sys.path.insert(0, 'C:\EnergyPlusV9-6-0')  # add E-Plus directory to path to be able to import API
+sys.path.insert(0, 'C:\EnergyPlusV23-1-0')  # add E-Plus directory to path to be able to import API
 from pyenergyplus.api import EnergyPlusAPI #import EnergyPlus library
 
 def my_callback_function(state):
     # global variables are necessary as the callback function takes only one input: state
-    global heatingSch_hndl, coolingSch_hndl,indoorT_hndl,outdoorT_hndl,heat_coil_hndl, handleDone
+    global heatingSch_hndl, coolingSch_hndl,heatingSP_hndl,coolingSP_hndl,indoorT_hndl,outdoorT_hndl,heat_coil_hndl, handleDone
     # get handles
     if not handleDone:
         if api.exchange.api_data_fully_ready(state):
             heatingSch_hndl = api.exchange.get_actuator_handle(state, "Schedule:Constant", "Schedule Value", "HeatingSch")
             coolingSch_hndl = api.exchange.get_actuator_handle(state, "Schedule:Constant", "Schedule Value", "CoolingSch")
+            heatingSP_hndl = api.exchange.get_actuator_handle(state, "Schedule:Constant", "Schedule Value", "hSP")
+            coolingSP_hndl = api.exchange.get_actuator_handle(state, "Schedule:Constant", "Schedule Value", "cSP")
             outdoorT_hndl = api.exchange.get_variable_handle(state,'Site Outdoor Air Drybulb Temperature', 'Environment')
             indoorT_hndl = api.exchange.get_variable_handle(state,'Zone Mean Air Temperature', 'Zone1')
             heat_coil_hndl = api.exchange.get_meter_handle(state,'HeatingCoils:EnergyTransfer')
@@ -33,6 +35,10 @@ def my_callback_function(state):
     # read meters
     heating_coils = api.exchange.get_meter_value(state, heat_coil_hndl)
     
+    # change setpoints
+    api.exchange.set_actuator_value(state, heatingSP_hndl, 20)
+    api.exchange.set_actuator_value(state, coolingSP_hndl, 25)
+    
     # actuate
     if month < 4 or month > 10:
         api.exchange.set_actuator_value(state, heatingSch_hndl, 1)
@@ -40,11 +46,15 @@ def my_callback_function(state):
     else:
         api.exchange.set_actuator_value(state, heatingSch_hndl, 0)
         api.exchange.set_actuator_value(state, coolingSch_hndl, 1)
+    
+    
 
 
 # initializations
 heatingSch_hndl = 0
 coolingSch_hndl = 0
+heatingSP_hndl = 0
+coolingSP_hndl = 0
 outdoorT_hndl = 0
 indoorT_hndl = 0
 heat_coil_hndl = 0
@@ -52,16 +62,25 @@ handleDone = False
 
 # initialize EPlus
 api = EnergyPlusAPI()
+
 #instance of the simulation
 state = api.state_manager.new_state() 
+
 # energyplus model calling point, callback function
 api.runtime.callback_begin_system_timestep_before_predictor(state , my_callback_function)
+
 # run EPlus
 epwFile = 'ON_OTTAWA-INTL-ONT_716280_19.epw'
 idfFile = 'Shoebox.idf'
+output_folder = 'out'
 # -x short form to run expandobjects for HVACtemplates. see EnergyPlusEssentials.pdf p16
-cmd_args = ['-w',epwFile, '-d', 'out','-x',idfFile]
+cmd_args = ['-w',epwFile, '-d', output_folder,'-x',idfFile]
 api.runtime.run_energyplus(state,cmd_args)
+
+# delete state after simulation to free the memory
+api.state_manager.delete_state(state)
+
+print("Handles done: {}".format(handleDone))
 
 
 
